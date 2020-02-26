ade46876dee8a3b5ee276d0f835f70e0113e36ed
huangmj3 <18292493938@163.com>
2020-02-26 16:15:35 +0800

    add modules

--- user-api/src/main/java/com/example/demo/DemoApplication.java
+++ user-api/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package huangmaojie.duomall.user;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
--- user-api/src/test/java/com/example/demo/DemoApplicationTests.java
+++ user-api/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package huangmaojie.duomall.user;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
