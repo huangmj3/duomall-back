329c8d54e7e4765b306197b8af205372dbaa90a7
huangmj3 <18292493938@163.com>
2020-03-24 15:43:54 +0800

    初始化eureka 服务器

--- server/src/test/java/com/huangmaojie/duomall/server/ServerApplicationTests.java
+++ server/src/test/java/com/huangmaojie/duomall/server/ServerApplicationTests.java
@@ -1,13 +1,13 @@
-package huangmaojie.eureka.server;
+package com.huangmaojie.duomall.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class ServerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
--- user-server/src/main/java/com/huangmaojie/duomall/user/Application.java
+++ user-server/src/main/java/com/huangmaojie/duomall/user/Application.java
@@ -3,12 +3,13 @@ package com.huangmaojie.duomall.user;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author huangmaojie
  * @date 2020.2.26
  */
-//@EnableEurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 @MapperScan("com.huangmaojie.duomall.user.mapper")
 public class Application {
