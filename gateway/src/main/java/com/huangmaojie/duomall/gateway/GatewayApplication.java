5e7ccf75481008269ff04b8399f04a46ea83a96c
huangmj3 <18292493938@163.com>
2020-03-26 23:22:49 +0800

    重构eureka服务器

--- gateway/src/main/java/com/huangmaojie/duomall/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/huangmaojie/duomall/gateway/GatewayApplication.java
@@ -1,10 +1,12 @@
-package com.huangmaojie.duomall.gateway;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableEurekaServer
+@EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
 public class GatewayApplication {
 
---
+++ gateway/src/main/java/com/huangmaojie/duomall/gateway/config/WebFilter.java
@@ -0,0 +1,8 @@
+package com.huangmaojie.duomall.gateway.filter;
+
+/**
+ * @author huangmaojie
+ * @date 2020/3/26
+ */
+public class WebFilter {
+}
