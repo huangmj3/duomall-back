ab964d58c9fc12575a5d995843db56096de6b36b
huangmj3 <18292493938@163.com>
2020-03-28 13:00:20 +0800

    秒杀商品和配置文件修改

--- goods-server/src/main/java/com/huangmaojie/duomall/goods/service/impl/SpikeServiceImpl.java
+++ goods-server/src/main/java/com/huangmaojie/duomall/goods/service/impl/SpikeServiceImpl.java
@@ -1,8 +1,14 @@
 package com.huangmaojie.duomall.goods.service.impl;
 
+import com.huangmaojie.duomall.goods.service.SpikeService;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
 /**
  * @author huangmaojie
  * @date 2020/3/28
  */
-public class SpikeServiceImpl {
+@Service
+@Validated
+public class SpikeServiceImpl implements SpikeService {
 }
