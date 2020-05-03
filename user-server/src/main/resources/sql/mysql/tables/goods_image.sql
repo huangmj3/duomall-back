e9e2c3e99b68d451a5778beedf0710cdcee1f934
huangmj3 <18292493938@163.com>
2020-05-03 18:52:24 +0800

    数据库重构

--- user-server/src/main/resources/sql/mysql/tables/goods_image.sql
+++ user-server/src/main/resources/sql/mysql/tables/goods_image.sql
@@ -2,10 +2,10 @@
 use duomall;
 create table if not exists goods_image
 (
-    `id`           varchar(36)  not null comment '主键id',
-    `goods_id`     varchar(36)  not null comment '对应商品id',
-    `image`        varchar(255) not null comment '商品图片',
-    `type`         int          not null comment '图片类型,0-展示用图片,1-细节用图片',
+    `id`           varchar(36)          not null comment '主键id',
+    `goods_id`     varchar(36)          not null comment '对应商品id',
+    `image`        varchar(255)         not null comment '商品图片',
+    `type`         int        default 0 not null comment '图片类型,0-展示用图片,1-细节用图片',
     `status`       tinyint    default 1 comment '0-关闭，1-正常，2-禁用',
     `gmt_create`   timestamp  default current_timestamp comment '创建时间',
     `gmt_modified` timestamp  default current_timestamp on update current_timestamp comment '修改时间',
